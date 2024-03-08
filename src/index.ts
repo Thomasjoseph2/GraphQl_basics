750b56063d771498ec9795c3db337ae338f240c1
Thomas joseph <tj089502@gmail.com>
2024-03-08 18:47:31 +0530

    need some reffactoring

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,43 @@ import { expressMiddleware } from '@apollo/server/express4';
 import cors from 'cors';
 import express from 'express';
 import dotenv from 'dotenv'
+import { prismaClient } from './lib/db';
 dotenv.config()
 async function Init() {
     const app = express()
     app.use(express.json())
     const graphQlServer = new ApolloServer({
-        typeDefs: `type Query{
+        typeDefs: `
+        type Query{
             hellow:String
             say(name:String):String
-        }`,
+        }
+
+        type Mutation{
+            createUser(firstName:String!, lastName:String!,email:String! ,password:String!):Boolean
+        }
+    
+        `,
         resolvers: {
             Query: {
                 hellow: () => `hey there im graphql`,
                 say: (_, { name }: { name: String }) => `hey ${name}`
+            },
+            Mutation: {
+                createUser: async (_, { firstName, lastName, email, password }: { firstName: string, lastName: string, email: string, password: string }) => {
+                    await prismaClient.user.create({
+                        data: {
+                            email,
+                            firstName,
+                            lastName,
+                            password,
+                            salt: 'random_salt'
+                        }
+                    })
+                    return true
+                }
             }
+
         }
     })
 
